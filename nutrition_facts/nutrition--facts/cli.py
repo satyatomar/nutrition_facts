5f67bfaa504ecd02675a52e4eba0bcb2c92ad232
satyatomar <48763031+satyatomar@users.noreply.github.com>
2020-08-16 17:45:14 +0530

    Add files via upload

---
+++ nutrition_facts/nutrition--facts/cli.py
@@ -0,0 +1,12 @@
+
+from utils import *
+
+
+def main():
+        get_nutrition()
+
+        
+
+
+if __name__ == "__main__":
+	main()
---
+++ nutrition_facts/nutrition--facts/utils.py
@@ -0,0 +1,17 @@
+#nutrition_facts
+
+def get_nutrition(item=None):
+
+        if item is None:
+                print("Enter a valid entry")
+
+        elif item == "mango":
+                print("123321")
+
+        else:
+                print("Thanks")
+
+
+
+
+
