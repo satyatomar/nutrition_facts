4de2cd4a9a55fd92b1351c77b1558d5ddad983c5
satyatomar <48763031+satyatomar@users.noreply.github.com>
2020-08-16 18:59:14 +0530

    changes in utils

--- nutrition_facts/nutrition--facts/utils.py
+++ nutrition_facts/nutrition--facts/utils.py
@@ -6,7 +6,9 @@ def get_nutrition(item=None):
                 print("Enter a valid entry")
 
         elif item == "mango":
-                print("123321")
+                print("Serving size: 1 ounce (28 g)\nCalories 163; Calories from Fat 119\
+                       Total Fat 2g 3%\nTotal Carbohydrate 12g 4%\nCholesterol 10mg 4%\nDietary Fiber 0g 0%\
+                       Sodium 125mg 5%\nSugars 12g\nProtein 11g\nVitamin A 10%\nCalcium	30%\nVitamin C 0%\nIron 0%")
 
         else:
                 print("Thanks")
