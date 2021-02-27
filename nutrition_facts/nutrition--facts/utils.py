d55de5ad757afce2f6834d610c2ed13c25947840
TurboWafflz <turbowafflz@gmail.com>
2021-02-27 16:46:21 -0500

    Return instead of printing and raise exception on invalid food instead of printing error

--- nutrition_facts/nutrition--facts/utils.py
+++ nutrition_facts/nutrition--facts/utils.py
@@ -1,14 +1,28 @@
 #nutrition_facts
 
+class invalidFood(Exception):
+	pass
+
 def get_nutrition(item=None):
 
         if item is None:
-                print("Enter a valid entry")
+                raise invalidFood("You have entered an invalid food item")
 
         elif item == "mango":
-                print("Serving size: 1 ounce (28 g)\nCalories 163; Calories from Fat 119\
-                       Total Fat 2g 3%\nTotal Carbohydrate 12g 4%\nCholesterol 10mg 4%\nDietary Fiber 0g 0%\
-                       Sodium 125mg 5%\nSugars 12g\nProtein 11g\nVitamin A 10%\nCalcium	30%\nVitamin C 0%\nIron 0%")
+                return {'Serving size': '1 ounce (28 g)',
+						'Calories': '163',
+						'Calories from Fat': '119',
+						'Total Fat': '2g 3%',
+						'Total Carbohydrate': '12g 4%',
+						'Cholesterol': '10mg 4%',
+						'Dietary Fiber': '0g 0%',
+						'Sodium': '125mg 5%',
+						'Sugars': '12g',
+						'Protein': '11g',
+						'Vitamin A': '10%',
+						'Calcium': '30%',
+						'Vitamin C': '0%',
+						'Iron': '0%'}
 
         else:
                 print("Thanks")
